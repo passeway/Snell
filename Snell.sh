deedfdd9d9c1aa6ff562192624a17f54e8e39c50
passeway <164740303+passeway@users.noreply.github.com>
2024-04-08 14:48:37 +0800

    Update Snell.sh

--- Snell.sh
+++ Snell.sh
@@ -3,62 +3,80 @@
 # 更新系统包和升级
 apt-get update && apt-get -y upgrade
 
-# 安装 Docker
-curl -fsSL https://get.docker.com | bash -s docker
+# 安装必要的软件包
+apt-get install -y unzip wget curl
 
-# 判断并卸载不同版本的 Docker Compose
-if [ -f "/usr/local/bin/docker-compose" ]; then
-    sudo rm /usr/local/bin/docker-compose
+# 下载 Snell 服务器文件
+SNELL_VERSION="v4.0.1"
+ARCH=$(arch)
+SNELL_URL=""
+INSTALL_DIR="/usr/local/bin"
+SYSTEMD_SERVICE_FILE="/lib/systemd/system/snell.service"
+CONF_DIR="/etc/snell"
+CONF_FILE="$CONF_DIR/snell-server.conf"
+
+if [[ $ARCH == "aarch64" ]]; then
+    SNELL_URL="https://dl.nssurge.com/snell/snell-server-$SNELL_VERSION-linux-aarch64.zip"
+else
+    SNELL_URL="https://dl.nssurge.com/snell/snell-server-$SNELL_VERSION-linux-amd64.zip"
 fi
 
-if [ -d "$HOME/.docker/cli-plugins/" ]; then
-    rm -rf $HOME/.docker/cli-plugins/
-fi
+# 下载 Snell 服务器文件
+wget $SNELL_URL -O snell-server.zip
 
-# 安装 Docker Compose 插件
-apt-get install docker-compose-plugin -y
+# 解压缩文件到指定目录
+sudo unzip -o snell-server.zip -d $INSTALL_DIR
 
-# 创建所需目录
-mkdir -p /root/snelldocker/snell-conf
+# 删除下载的 zip 文件
+rm snell-server.zip
+
+# 赋予执行权限
+chmod +x $INSTALL_DIR/snell-server
 
 # 生成随机端口和密码
 RANDOM_PORT=$(shuf -i 30000-65000 -n 1)
 RANDOM_PSK=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 20)
 
-# 设置 Snell 服务器的 URL，考虑 ARM 架构
-SNELL_URL="https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-amd64.zip"
-if [[ $(arch) == "aarch64" ]]; then
-    SNELL_URL="https://dl.nssurge.com/snell/snell-server-v4.0.1-linux-aarch64.zip"
-fi
+# 创建配置文件目录
+mkdir -p $CONF_DIR
 
-# 创建 docker-compose.yml
-cat > /root/snelldocker/docker-compose.yml << EOF
-version: "3.8"
-services:
-  snell:
-    image: accors/snell:latest
-    container_name: snell
-    restart: always
-    network_mode: host
-    volumes:
-      - ./snell-conf/snell.conf:/etc/snell-server.conf
-    environment:
-      - SNELL_URL=$SNELL_URL
-EOF
-
-# 创建 snell.conf 配置文件
-cat > /root/snelldocker/snell-conf/snell.conf << EOF
+# 创建配置文件
+cat > $CONF_FILE << EOF
 [snell-server]
 listen = ::0:$RANDOM_PORT
 psk = $RANDOM_PSK
 ipv6 = false
 EOF
 
-# 切换目录
-cd /root/snelldocker
+# 创建 Systemd 服务文件
+cat > $SYSTEMD_SERVICE_FILE << EOF
+[Unit]
+Description=Snell Proxy Service
+After=network.target
 
-# 拉取并启动 Docker 容器
-docker compose pull && docker compose up -d
+[Service]
+Type=simple
+User=nobody
+Group=nogroup
+LimitNOFILE=32768
+ExecStart=/usr/local/bin/snell-server -c $CONF_FILE
+AmbientCapabilities=CAP_NET_BIND_SERVICE
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=snell-server
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# 重载 Systemd 配置
+sudo systemctl daemon-reload
+
+# 开机自启动 Snell
+sudo systemctl enable snell
+
+# 启动 Snell 服务
+sudo systemctl start snell
 
 # 获取本机IP地址
 HOST_IP=$(curl -s http://checkip.amazonaws.com)
