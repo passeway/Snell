efbf70759c4b0a8c9d09beef1883cc640aa9d8a8
passeway <164740303+passeway@users.noreply.github.com>
2026-05-04 18:24:20 +0800

    Delete check.js

--- check.js
+++
@@ -1,8 +0,0 @@
-let obj = JSON.parse($response.body);
-
-if (obj && obj.response && obj.response.head) {
-  obj.response.head.respCode = "0000";
-  obj.response.head.respMsg = "成功";
-}
-
-$done({ body: JSON.stringify(obj) });
